0e09c1fa8ecb45529ad2658576729a338e490b7f
T-Keichan <56142065+T-Keichan@users.noreply.github.com>
2020-01-14 17:48:50 +0900

    Create thanks.php

---
+++ thanks.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>登録完了</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="content">
+        <header>
+            <div class="mein-name">
+                <h2>ひとこと掲示板Onetweet</h2>
+            </div>
+        </header>
+        <div>
+            <h2 class="heading">ユーザー登録ありがとうございます！</h2>
+            <h3 class="heading">下のボタンからログイン画面へ移動し、ログインしてください。</h3>
+        </div>
+        <a href="login.php"><button>ログイン画面へ</button></a>
+    </div>
+</body>
+
+</html>
